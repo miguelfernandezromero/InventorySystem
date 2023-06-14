187409b9618cff104d4b582844be330c02159443
miguelfernandezromero <miguelfernandezromero@gmail.com>
2023-06-14 11:51:07 -0500

    Inital Scaffolding

---
+++ src/main/java/com/company/inventorysystem/InventorysystemApplication.java
@@ -0,0 +1,14 @@
+package com.company.inventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorysystemApplication.class, args);
+		System.out.println("hola mundo");
+	}
+
+}
